1989963909de98f7695ebc12ad131d16fb6c0eb4
mohan <mohan14242@gmail.com>
2024-07-29 16:24:38 +0530

    mohan

---
+++ unit_test/test_main.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import pytest
+from fastapi.testclient import TestClient
+
+# Add the `src` directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
+from main import app  # Import the FastAPI application from src
+
+client = TestClient(app)
+
+def test_get_name():
+    response = client.get("/name")
+    assert response.status_code == 200
+    assert response.json() == "this is my name"
