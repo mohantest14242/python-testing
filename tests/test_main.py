fb5a3379430cc81a61d19ab4d1aeacd5a481ac38
mohan <mohan14242@gmail.com>
2024-07-29 16:51:14 +0530

    mohan

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,16 @@
-import sys
 import os
+import sys
 from fastapi.testclient import TestClient
-sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'../../src')))
+
+# Adjust the import path
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '../../src')))
+
 from main import app
+
 client = TestClient(app)
+
+
 def test_get_name():
     response = client.get("/name")
     assert response.status_code == 200
--- unit_test/test_main.py
+++ unit_test/test_main.py
@@ -1,13 +1,15 @@
-import sys
 import os
+import sys
 from fastapi.testclient import TestClient
 
-# Add the `src` directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+# Adjust the import path
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '../../src')))
 
-from main import app  # Import the FastAPI application from src
+from main import app
 
 client = TestClient(app)
+
 
 def test_get_name():
     response = client.get("/name")
