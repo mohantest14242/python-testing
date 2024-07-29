bc64e6c71a8dabe5fb21bd926d9c52f12ff535a6
mohan <mohan14242@gmail.com>
2024-07-29 16:47:02 +0530

    mohan

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,7 @@ import os
 from fastapi.testclient import TestClient
 from main import app
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'../../src')))
+from main import app
 client = TestClient(app)
 def test_get_name():
     response = client.get("/name")
