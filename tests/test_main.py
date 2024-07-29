68ca6c8f25bb3e3e143b1d016b43d53f52d8b667
mohan <mohan14242@gmail.com>
2024-07-29 16:11:30 +0530

    mohan

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ import os
 # Add the directory containing `src` to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
-from src.main import app  # Import the FastAPI application
+from .src.main import app  # Import the FastAPI application
 
 from fastapi.testclient import TestClient
 
