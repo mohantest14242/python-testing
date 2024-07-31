d5db7f680998b77b787af9ade52d4fd3ef157a7c
mohan <mohan14242@gmail.com>
2024-07-31 18:41:14 +0530

    mohan

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 
 # Adjust the import path
 sys.path.insert(0, os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../../src')))
+    os.path.dirname(__file__), 'actions/src')))
 
 from main import app
 
